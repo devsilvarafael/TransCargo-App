fda5925b9026b564c072213b06b818ee6623b069
Rafael Silva <devsilva.rafael@gmail.com>
2023-04-22 20:02:36 -0300

    refactor(DefaultLayout): refactoring Default Layout style and add Avatar.

--- src/app/layouts/DefaultLayout.tsx
+++ src/app/layouts/DefaultLayout.tsx
@@ -10,6 +10,10 @@ import {
   Paper,
   Divider,
   Box,
+  Avatar,
+  Stack,
+  Typography,
+  Badge,
 } from "@mui/material";
 import { ReactNode } from "react";
 import Image from "next/image";
@@ -18,12 +22,41 @@ import DashboardIcon from "@/app/assets/dashboard-icon.svg";
 import TruckIcon from "@/app/assets/truck-icon.svg";
 import ManagementIcon from "@/app/assets/gerenciar-icon.svg";
 import SettingsIcon from "@/app/assets/settings-icon.svg";
+import styled from "@emotion/styled";
 
 interface DefaultLayoutProps {
   children: ReactNode;
   leftSideBar?: boolean;
   rightSideBar?: boolean;
 }
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    backgroundColor: "#44b700",
+    color: "#44b700",
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    "&::after": {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      border: "1px solid currentColor",
+      content: '""',
+    },
+  },
+  "@keyframes ripple": {
+    "0%": {
+      transform: "scale(.8)",
+      opacity: 1,
+    },
+    "100%": {
+      transform: "scale(2.4)",
+      opacity: 0,
+    },
+  },
+}));
 
 const leftSideBarOptions = [
   { id: 0, label: "Dashboard", icon: DashboardIcon },
@@ -48,48 +81,123 @@ export default function DefaultLayout({
       }}
     >
       {leftSideBar && (
-        <Drawer variant="permanent" sx={{ flexGrow: 1, maxWidth: 350 }}>
+        <Drawer
+          variant="permanent"
+          anchor="left"
+          sx={{
+            width: 200,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: 329,
+              boxSizing: "border-box",
+              border: "none",
+              boxShadow: 3,
+            },
+            mr: 4,
+          }}
+        >
           <Box
             sx={{
               display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
+              justifyContent: "space-between",
               flexDirection: "column",
               width: "100%",
-              minWidth: 300,
+              height: "100%",
             }}
           >
-            <Image
-              src="/transcargo-icon.svg"
-              alt="logo"
-              width={60}
-              height={60}
-            />
+            <Box
+              sx={{
+                height: "100%",
 
-            <Divider flexItem sx={{ marginTop: 2 }} />
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <Image
+                src="/transcargo-icon.svg"
+                alt="logo"
+                width={60}
+                height={60}
+              />
 
-            <List>
-              {leftSideBarOptions.map((item) => (
-                <ListItem key={item.id} disableGutters>
-                  <ListItemButton>
-                    <ListItemIcon>
-                      <Image
-                        src={item.icon}
-                        alt={item.label}
-                        width={20}
-                        height={20}
-                      />
-                    </ListItemIcon>
-                    <ListItemText primary={item.label} />
-                  </ListItemButton>
-                </ListItem>
-              ))}
-            </List>
+              <List>
+                {leftSideBarOptions.map((item) => (
+                  <ListItem key={item.id} disableGutters alignItems="center">
+                    <ListItemButton alignItems="center">
+                      <ListItemIcon>
+                        <Image
+                          src={item.icon}
+                          alt={item.label}
+                          width={20}
+                          height={20}
+                        />
+                      </ListItemIcon>
+                      <ListItemText primary={item.label} />
+                    </ListItemButton>
+                  </ListItem>
+                ))}
+              </List>
+            </Box>
+
+            <Stack
+              direction="row"
+              spacing={2}
+              mb={8}
+              alignItems="center"
+              justifyContent="center"
+            >
+              <StyledBadge
+                overlap="circular"
+                anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+                variant="dot"
+              >
+                <Avatar
+                  src="https://picsum.photos/id/237/200/300"
+                  alt="username"
+                />
+              </StyledBadge>
+              <Typography>Harry Potter</Typography>
+            </Stack>
           </Box>
         </Drawer>
       )}
 
-      <Box mt={4}>{children}</Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          flexDirection: "row",
+          width: "100%",
+        }}
+      >
+        <Box sx={{ ml: 16, mt: 8 }}>{children}</Box>
+        {rightSideBar && (
+          <Drawer
+            variant="permanent"
+            anchor="right"
+            sx={{
+              width: 429,
+              flexShrink: 0,
+              "& .MuiDrawer-paper": {
+                width: 429,
+                boxSizing: "border-box",
+                border: "none",
+                boxShadow: 3,
+              },
+              mr: 4,
+            }}
+          >
+            Teste
+          </Drawer>
+        )}
+      </Box>
     </Paper>
   );
 }
+
+// Fix eslint error: required default props
+
+DefaultLayout.defaultProps = {
+  leftSideBar: false,
+  rightSideBar: false,
+};
