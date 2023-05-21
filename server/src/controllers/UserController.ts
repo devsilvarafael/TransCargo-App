e55e771eb3e71cab28f65f625abae422e9ddbe16
Rafael Silva <devsilva.rafael@gmail.com>
2023-05-21 20:21:16 -0300

    feat: Add password encrypting on create new user, now User table have new relationships with Driver and Customer roles.

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -1,18 +1,12 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import { validateCNPJ } from "../utils/validateCNPJ";
+
+const bcrypt = require("bcrypt");
 
 const prisma = new PrismaClient();
 
-async function main() {
-    await prisma.user.create({
-        data: {
-            name: 'Rafael Silva',
-            email: 'admin@transcargo.com',
-            password: 'admin',
-            role: "adm"
-        },
-    })
-}
+
 
 export const getAllUsers = async (_req: Request, res: Response) => {
     try {
@@ -25,8 +19,52 @@ export const getAllUsers = async (_req: Request, res: Response) => {
 
 export const createUser = async (req: Request, res: Response) => {
     const { name, email, role, password } = req.body;
+
+    const passwordHash = await bcrypt.hash(password, 10)
+
     try {
-        const user = await prisma.user.create({ data: { name, email, role, password } });
+        const user = await prisma.user.create({ data: { name, email, role, password: passwordHash } });
+
+        if(role === "driver") {
+            const { cnh } = req.body;
+
+            await prisma.driver.create({
+                data: {
+                    cnh,
+                    user: {
+                        connect: {
+                            id: user.id,
+                        },
+                    },
+                },
+            });
+
+            console.log('Criou motorista')
+
+        }
+
+        if(role === "customer") {
+            const { cnpj } = req.body;
+
+            const isValid = validateCNPJ(cnpj);
+
+            if (!isValid) {
+                return res.status(400).json({ error: 'CNPJ inválido' });
+            }
+
+            await prisma.customer.create({
+                data: {
+                    cnpj,
+                    user: {
+                        connect: {
+                            id: user.id
+                        }
+                    }
+                }
+            })
+
+        }
+
         res.status(201).json(user);
     } catch (error) {
         res.status(500).json({ message: 'Internal server error' });
@@ -36,11 +74,28 @@ export const createUser = async (req: Request, res: Response) => {
 export const deleteUser = async (req: Request, res: Response) => {
     const { id } = req.params;
     try {
-        await prisma.user.delete({ where: { id: Number(id) } });
+        // Excluir o registro de Driver associado ao usuário, se existir
+        await prisma.driver.deleteMany({
+            where: {
+                userId: Number(id),
+            },
+        });
+
+        // Excluir o registro de Customer associado ao usuário, se existir
+        await prisma.customer.deleteMany({
+            where: {
+                userId: Number(id),
+            },
+        });
+
+        // Excluir o usuário
+        await prisma.user.delete({
+            where: {
+                id: Number(id),
+            },
+        });
         res.json({ message: 'User deleted successfully' });
     } catch (error) {
         res.status(500).json({ message: 'Internal server error' });
     }
 };
-
-main();
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from 'cors';
 
 import userRouter from "./routes/userRoutes";
+import auth from "./routes/auth";
 
 const app = express();
 const PORT = 8080;
@@ -11,6 +12,7 @@ app.use(cors());
 
 
 app.use('/api', userRouter);
+app.use('/api', auth);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
