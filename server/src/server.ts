d97f65c0dd4489df48c75f61a39abf085fc6b493
Rafael Silva <devsilva.rafael@gmail.com>
2023-05-25 01:03:43 -0300

    chore: Turn routes use in server to array.

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 
 import userRouter from "./routes/userRoutes";
 import auth from "./routes/auth";
+import truckRouter from "./routes/truckRoutes";
+import driverRouter from "./routes/driverRoutes";
 
 const app = express();
 const PORT = 8080;
@@ -11,8 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 
-app.use('/api', userRouter);
-app.use('/api', auth);
+app.use('/api', [userRouter, truckRouter, driverRouter, auth]);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
